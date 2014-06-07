8b3770d982c99d9e9fbc6536bbfc86a0a697b281
rchavarria <txingo@gmail.com>
2014-06-08 00:26:07 +0200

    add gulp task to handle scripts

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify');
+
+gulp.task('scripts', function() {
+    return gulp.src('src/scripts/**/*.js')
+        .pipe(jshint('.jshintrc'))
+        .pipe(jshint.reporter('default'))
+        .pipe(gulp.dest('dist/assets/js'))
+        .pipe(rename({suffix: '.min'}))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist/assets/js'));
+});
