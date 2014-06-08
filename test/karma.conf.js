1508cab5f9fb58bdd1bf61827686972de5ea41c0
rchavarria <txingo@gmail.com>
2014-06-08 12:41:57 +0200

    configure karma to use CommonJS as preprocessor, but tests don't run yet

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -7,7 +7,7 @@ module.exports = function(config) {
   config.set({
     basePath: '..',
 
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'commonjs'],
 
     // list of files / patterns to load in the browser
     files: [
@@ -21,14 +21,14 @@ module.exports = function(config) {
     ],
 
     preprocessors: {
-//      'client/*.js': ['commonjs'],
-//      'test/client/*.js': ['commonjs']
+      'src/scripts/**/*.js': ['commonjs'],
+      'test/scripts/**/*.js': ['commonjs']
     },
 
     // use dots reporter, as travis terminal does not support escaping sequences
     // possible values: 'dots', 'progress'
     // CLI --reporters progress
-    reporters: ['progress', 'junit'],
+    reporters: ['progress'],
 //    reporters: ['progress', 'junit'],
 
 //    junitReporter: {
@@ -47,7 +47,7 @@ module.exports = function(config) {
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     // CLI --log-level debug
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_DEBUG,
 
     // enable / disable watching file and executing tests whenever any file changes
     // CLI --auto-watch --no-auto-watch
@@ -63,7 +63,7 @@ module.exports = function(config) {
     // - IE (only Windows)
     // CLI --browsers Chrome,Firefox,Safari
 //    browsers: [process.env.TRAVIS ? 'Firefox' : 'Chrome'],
-    browsers: [],
+    browsers: ['PhantomJS'],
 
     // If browser does not capture in given timeout [ms], kill it
     // CLI --capture-timeout 5000
@@ -71,7 +71,7 @@ module.exports = function(config) {
 
     // Auto run tests on start (when browsers are captured) and exit
     // CLI --single-run --no-single-run
-    singleRun: false,
+    singleRun: true,
 
     // report which specs are slower than 500ms
     // CLI --report-slower-than 500
@@ -79,6 +79,7 @@ module.exports = function(config) {
 
     plugins: [
       'karma-jasmine',
+      'karma-phantomjs-launcher',
 //      'karma-chrome-launcher',
 //      'karma-firefox-launcher',
 //      'karma-junit-reporter',
