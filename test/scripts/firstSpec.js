a3cbcb79053a3313f141ed55e3899d3e4e488ba3
rchavarria <txingo@gmail.com>
2014-06-09 09:27:22 +0200

    add a gulp task to run tests with mocha

--- test/scripts/firstSpec.js
+++ test/scripts/firstSpec.js
@@ -4,4 +4,9 @@ describe('First test', function () {
 	it('is run with mocha and expect.js', function () {
 		expect(1).to.be(1);
 	});
+
+	it('requires modules from production code', function() {
+		var module = require('../../src/scripts/testRequire');
+		expect(module.testRequire()).to.be.ok();
+	})
 });
