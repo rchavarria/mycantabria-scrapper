130c00115156b793cacf6ca44aa2e588e996fb81
rchavarria <txingo@gmail.com>
2014-06-11 08:48:01 +0200

    start working on a solution with promises

---
+++ src/scripts/scrapperPromises.js
@@ -0,0 +1,38 @@
+var request = require('request'),
+    cheerio = require('cheerio');
+
+var options = {
+    url: 'http://mycantabria.com',
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0'
+    }
+};
+
+function responseHandler(err, response, body) {
+    if (err) throw err;
+
+    var $ = cheerio.load(body);
+    var h2s = [];
+
+    $('h2').each(function () {
+        h2s.push($(this).text());
+    });
+
+    return h2s;
+}
+
+this.scrap = function (site) {
+    options.url = site;
+    var r = request(options),
+        chunks = [];
+
+    r.on('data', function (chunk) {
+        chunks.push(chunk);
+    });
+    r.on('end', function () {
+        console.log('-------------------------------');
+        var body = chunks.join('');
+        console.log('body: ' + body);
+        console.log('received: ' + chunks.length + ' chunks of data');
+    });
+}
