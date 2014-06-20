f9fad58d4e46c8d064df8bf72997f4844cbe06b8
rchavarria <txingo@gmail.com>
2014-06-20 20:17:59 +0200

    convert to spaces

--- test/scripts/firstSpec.js
+++ test/scripts/firstSpec.js
@@ -1,12 +1,12 @@
 describe('First test', function () {
-	var expect = require('chai').expect;
+    var expect = require('chai').expect;
 
-	it('runs with mocha and chai', function () {
-		expect(1).equals(1);
-	});
+    it('runs with mocha and chai', function () {
+        expect(1).equals(1);
+    });
 
-	it('requires modules from production code', function() {
-		var module = require('../../src/scripts/testRequire');
-		expect(module.testRequire()).to.equal(true);
-	})
+    it('requires modules from production code', function() {
+        var module = require('../../src/scripts/testRequire');
+        expect(module.testRequire()).to.equal(true);
+    })
 });
