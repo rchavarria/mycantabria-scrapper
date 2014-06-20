4b53405756d70aeb15546bc34196bb35179172a1
rchavarria <txingo@gmail.com>
2014-06-20 20:16:17 +0200

    refactor first test to use chai instead of expect

--- test/scripts/firstSpec.js
+++ test/scripts/firstSpec.js
@@ -1,12 +1,12 @@
 describe('First test', function () {
-	var expect = require('expect.js');
+	var expect = require('chai').expect;
 
-	it('is run with mocha and expect.js', function () {
-		expect(1).to.be(1);
+	it('runs with mocha and chai', function () {
+		expect(1).equals(1);
 	});
 
 	it('requires modules from production code', function() {
 		var module = require('../../src/scripts/testRequire');
-		expect(module.testRequire()).to.be.ok();
+		expect(module.testRequire()).to.equal(true);
 	})
 });
