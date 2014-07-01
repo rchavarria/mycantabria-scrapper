23d01c80bafdfcd002328c7c16508e40fc7ba570
rchavarria <txingo@gmail.com>
2014-07-02 00:48:45 +0200

    fix semicolon typo

--- test/scripts/factorySpec.js
+++ test/scripts/factorySpec.js
@@ -15,7 +15,7 @@ describe('module: Factory', function () {
             expect(factory.createPromise(1234)).to.eventually.equals(1234);
             expect(factory.createPromise('string value')).to.eventually.equals('string value');
             expect(factory.createPromise([1, 2, 3])).to.eventually.equals([1, 2, 3]);
-        })
+        });
 
     });
 });
