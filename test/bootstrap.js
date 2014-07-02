952061646b025dac374c191fe835512118b558b3
rchavarria <txingo@gmail.com>
2014-07-02 08:50:46 +0200

    add sinon-chai to the bootstrap file for chai

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,5 +1,9 @@
 global.chai = require('chai');
+global.sinon = require('sinon');
 global.expect = chai.expect;
 
 var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
+
+var sinonChai = require('sinon-chai');
+chai.use(sinonChai);
