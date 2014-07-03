183a1ae0cb60f7e384de7d62df594a7f9cb4ac83
rchavarria <txingo@gmail.com>
2014-07-04 01:20:04 +0200

    refactor: removed unusedd dependency

--- test/scripts/crawlerSpec.js
+++ test/scripts/crawlerSpec.js
@@ -1,6 +1,5 @@
 describe('module: Crawler', function () {
     var Q = require('q'),
-        request = require('request'),
 
         // local dependencies
         Crawler = require('../../src/scripts/crawler');
