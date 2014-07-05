82aeeef7667c250f4aad834fce57f34559495c7d
rchavarria <txingo@gmail.com>
2014-07-06 00:10:14 +0200

    add tests to learn a bit about how to use Chai As Promised library

---
+++ test/scripts/learn/chaiAsPromisedSpec.js
@@ -0,0 +1,23 @@
+describe('Chai As Promised API', function() {
+    var Q = require('q');
+    
+    it('checks values returned by promises', function() {
+        var value = 4,
+            promise = Q(function () { return value }).call();
+
+        // /!\ NOTICE the return statement /!\
+        return expect(promise).to.eventually.equals(value);
+    });
+
+    it('checks rejected promises', function() {
+        var deferred = Q.defer(),
+            promise = deferred.promise;
+
+        // test
+        deferred.reject('foo bar');
+
+        // assert
+        // /!\ NOTICE the return statement /!\
+        return expect(promise).to.be.rejected;
+    });
+})
