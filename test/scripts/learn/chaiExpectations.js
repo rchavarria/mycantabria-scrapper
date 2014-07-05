ee4d009d21013289f8c04cb8eba3f5449c69ed45
rchavarria <txingo@gmail.com>
2014-07-06 00:30:49 +0200

    add tests learning the #expect API of Chai

---
+++ test/scripts/learn/chaiExpectations.js
@@ -0,0 +1,16 @@
+describe('Chai', function() {
+	
+	describe('#expect', function() {
+		
+		describe('#equal', function() {
+			
+			it('expects a number to equal the same number', function() {
+				expect(2 + 2).to.equal(4);
+			});
+
+			it('expects arrays with the #members function', function() {
+				expect([1, 2, 3]).to.have.members([1, 2, 3]);
+			});
+		});
+	});
+});
