57df63c0a13393a99823e2b73356a9efca849574
rchavarria <txingo@gmail.com>
2014-07-06 12:48:11 +0200

    fix jshint errors

--- test/scripts/learn/chaiExpectations.js
+++ test/scripts/learn/chaiExpectations.js
@@ -1,16 +1,17 @@
+/*global describe, it, expect*/
 describe('Chai', function() {
-	
-	describe('#expect', function() {
-		
-		describe('#equal', function() {
-			
-			it('expects a number to equal the same number', function() {
-				expect(2 + 2).to.equal(4);
-			});
+    
+    describe('#expect', function() {
+        
+        describe('#equal', function() {
+            
+            it('expects a number to equal the same number', function() {
+                expect(2 + 2).to.equal(4);
+            });
 
-			it('expects arrays with the #members function', function() {
-				expect([1, 2, 3]).to.have.members([1, 2, 3]);
-			});
-		});
-	});
+            it('expects arrays with the #members function', function() {
+                expect([1, 2, 3]).to.have.members([1, 2, 3]);
+            });
+        });
+    });
 });
