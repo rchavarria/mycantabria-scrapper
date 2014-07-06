35b4744fc9e390becab1ffc7d2947016478296eb
rchavarria <txingo@gmail.com>
2014-07-06 13:13:56 +0200

    fix jshint errors

--- src/scripts/crawler.js
+++ src/scripts/crawler.js
@@ -1,15 +1,16 @@
+/*global module*/
 /**
  * Module that gets the content of the web pages that show properties by id.
  * Given a property id, this module gets its content and returns it inside
  * a promise
  */
-var Crawler = module.exports = function(Q, request) {
-    var Options = function (url) {
-            this.url = url,
-            this.headers = {
-                'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0'
-            }
+module.exports = function(Q, request) {
+    function Options(url) {
+        this.url = url;
+        this.headers = {
+            'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:29.0) Gecko/20100101 Firefox/29.0'
         };
+    }
 
     // constants
     this.MYCANTABRIA_PREFIX = 'http://mycantabria.com/inmueble.php?id_inmueble=';
@@ -27,19 +28,20 @@ var Crawler = module.exports = function(Q, request) {
         var i,
             options,
             deferred,
-            promises = [];
-
-        for (i = 0; i < ids.length; i++) {
-            deferred = this.Q.defer();
-            options = new Options(this.MYCANTABRIA_PREFIX + ids[i]);
-
-            this.request(options, function (err, response, body) {
+            promises = [],
+            responseHandler = function (err, response, body) {
                 if (err) {
                     deferred.reject(err);
                 } else {
                     deferred.resolve(body);
                 }
-            });
+            };
+
+        for (i = 0; i < ids.length; i++) {
+            deferred = this.Q.defer();
+            options = new Options(this.MYCANTABRIA_PREFIX + ids[i]);
+
+            this.request(options, responseHandler);
 
             promises.push(deferred.promise);
         }
