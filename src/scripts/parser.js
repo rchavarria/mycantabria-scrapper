433286f2294438d6b98425158065479d4326f352
rchavarria <txingo@gmail.com>
2014-07-09 12:22:27 +0200

    add comment to parser module

--- src/scripts/parser.js
+++ src/scripts/parser.js
@@ -1,4 +1,10 @@
 /*global module*/
+
+/**
+ * Parser module will parse property pages content to extract information
+ * and then populate a database or a set of files.
+ * Useful to save current properties shown in mycantabria.com
+ */
 module.exports = function(cheerio) {
 	this.cheerio = cheerio;
 
