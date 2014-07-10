9edc10088f252c2ffa0008b93a24baccfaf7a1aa
rchavarria <txingo@gmail.com>
2014-07-10 13:57:21 +0200

    add tests about how to load HTML into cheerio

--- test/scripts/learn/cheerioSpec.js
+++ test/scripts/learn/cheerioSpec.js
@@ -15,4 +15,35 @@ describe('Cheerio', function() {
             expect($('h2').html()).to.equal(newHtml);
         });
     });
+
+    describe('#API', function () {
+        var markup = '<ul id="fruits">' +
+            '<li class="apple">Apple</li>' +
+            '<li class="orange">Orange</li>' +
+            '<li class="pear">Pear</li>' +
+            '</ul>';
+
+        describe('##Loading', function () {
+
+            it('cheerio.load() is the preferred method', function () {
+                var $ = cheerio.load(markup);
+
+                return expect($('ul').html()).not.to.be.null;
+            });
+
+            it('loads HTML by passing it as the context', function () {
+                var $ = require('cheerio'),
+                    loaded = $('ul', markup);
+
+                return expect(loaded.html()).not.to.be.null; 
+            });
+
+            it('uses [htmlparser2 options](https://github.com/fb55/htmlparser2/wiki/Parser-options) to modify default parsing options', function () {
+                var options = { lowerCaseTags: true },
+                    $ = cheerio.load('<DIV><P>Hello</P></DIV>', options);
+
+                expect($('div').html()).to.equal('<p>Hello</p>');
+            });
+        });
+    });
 });
