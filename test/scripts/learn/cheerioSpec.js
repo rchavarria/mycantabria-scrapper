aab255dc5dc6d821355ab7341849df7743c65aa9
rchavarria <txingo@gmail.com>
2014-07-10 13:23:29 +0200

    new test: changing the html of an element

--- test/scripts/learn/cheerioSpec.js
+++ test/scripts/learn/cheerioSpec.js
@@ -1,12 +1,18 @@
 /*global describe, it, require, expect*/
-describe('Learning: Cheerio', function() {
-	var cheerio = require('cheerio');
+describe('Cheerio', function() {
+    var cheerio = require('cheerio');
 
-	describe('#basics', function () {
-		var $ = cheerio.load('<h2 class="title">Hello world</h2>');
+    describe('#basics', function () {
+        var $ = cheerio.load('<h2 class="title">Hello world</h2>');
 
-		it('selects an HTML element', function () {
-			expect($('h2').html()).to.equal('Hello world');
-		});
-	});
+        it('selects an HTML element', function () {
+            expect($('h2').html()).to.equal('Hello world');
+        });
+
+        it('can change the html of an HTML element', function () {
+            var newHtml = 'new message';
+            $('h2').html(newHtml);
+            expect($('h2').html()).to.equal(newHtml);
+        });
+    });
 });
