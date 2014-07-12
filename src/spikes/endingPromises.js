feae81b3da608dcbfd006c822cb938553396879f
rchavarria <txingo@gmail.com>
2014-07-12 20:54:06 +0200

    write down comments with the lesson learned

--- src/spikes/endingPromises.js
+++ src/spikes/endingPromises.js
@@ -9,14 +9,17 @@ function createPromise(value) {
 
 	setTimeout(callback, delay);
 
+	// LEARN Return the promise to wait for it to be resolved
 	return deferred.promise;
 }
 
-console.time('promises');
+console.time('promise resolved');
 console.time('func call');
 createPromise(12345)
+	// LEARN Use the promise returned and act in the 'then' method
 	.then(function (v) {
 		console.log('promise value:', v);
-		console.timeEnd('promises');
+		console.timeEnd('promise resolved');
+		// LEARN The nodejs process will finish here
 	});
 console.timeEnd('func call');
