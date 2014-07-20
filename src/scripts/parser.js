c137e9bb7b2bba1f7e0ccc8d83494cc6b1826538
rchavarria <txingo@gmail.com>
2014-07-20 12:38:13 +0200

    green. still red: the method should return a promise

--- src/scripts/parser.js
+++ src/scripts/parser.js
@@ -111,4 +111,20 @@ module.exports = function(cheerio) {
 
         return properties;
     };
+
+    this.parseWithPromises = function(propertyPagesContent) {
+        console.log('gonna parse', propertyPagesContent.length, 'pages');
+
+        var properties = [],
+            content,
+            i;
+
+        for (i = 0; i < propertyPagesContent.length; i++) {
+            content = propertyPagesContent[i];
+            properties.push(parseProperty(content));
+        }
+
+        return properties;
+    };
+
 };
