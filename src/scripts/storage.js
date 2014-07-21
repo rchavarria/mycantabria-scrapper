d508b1f7957d996a873b225e50cac5df524ea8cf
rchavarria <txingo@gmail.com>
2014-07-21 09:05:39 +0200

    refactor: remove duplication, properties folder

--- src/scripts/storage.js
+++ src/scripts/storage.js
@@ -2,8 +2,10 @@
 module.exports = function () {
     var fs = require('fs');
 
+    this.STORAGE_FOLDER = 'properties';
+
     this.save = function () {
-        fs.mkdir('properties');
+        fs.mkdir(this.STORAGE_FOLDER);
         console.log('saving');
     };
 };
--- test/scripts/storageSpec.js
+++ test/scripts/storageSpec.js
@@ -16,23 +16,23 @@ describe('module: Storage', function () {
         var fs = require('fs');
 
         afterEach(function () {
-            fs.exists('properties', function (exists) {
+            fs.exists(storage.STORAGE_FOLDER, function (exists) {
                 if (!exists) return;
 
-                fs.rmdir('properties');
+                fs.rmdir(storage.STORAGE_FOLDER);
             });
         });
 
         it('starts without a "properties" folder', function () {
             var dirs = fs.readdirSync('.');
-            expect(dirs).not.to.contain('properties');
+            expect(dirs).not.to.contain(storage.STORAGE_FOLDER);
         });
 
         it('creates a "properties" folder where to place all properties', function () {
             storage.save();
             var dirs = fs.readdirSync('.');
             console.log('dirs:', dirs);
-            expect(dirs).to.contain('properties');
+            expect(dirs).to.contain(storage.STORAGE_FOLDER);
         });
 
     });
