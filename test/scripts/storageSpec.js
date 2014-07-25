cdce75997d841937e2848a8e373d51859019c6ac
rchavarria <txingo@gmail.com>
2014-07-25 08:31:14 +0200

    delete red test for todo task

--- test/scripts/storageSpec.js
+++ test/scripts/storageSpec.js
@@ -79,10 +79,6 @@ describe('module: Storage', function () {
                 .catch(done);
         });
 
-        it('RED : TRY TO WRITE PROPERTIES TO FILES ASYNCHRONOUSLY', function () {
-            expect(1).to.equal(2);
-        });
-
     });
 
 });
